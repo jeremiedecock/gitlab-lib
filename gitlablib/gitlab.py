bd97e058cc3ddfc22f8f5025ee50c5138c647be8
Jeremie DECOCK <jd.jdhp@gmail.com>
2022-08-30 15:29:47 +0200

    Fix some bugs.

--- gitlablib/gitlab.py
+++ gitlablib/gitlab.py
@@ -23,37 +23,61 @@ class GitLab:
         return header_dict
 
 
-    def make_issue(self, project_id, title, labels, milestone_id, description):
+    def make_issue(self, gitlab_project_id, title, labels, milestone_id, description):
         # https://docs.gitlab.com/ee/api/issues.html#new-issue
-        #
-        # dict_keys(['id', 'iid', 'project_id', 'title', 'description', 'state', 'created_at', 'updated_at', 'closed_at', 'closed_by', 'labels', 'milestone', 'assignees', 'author', 'type', 'assignee', 'user_notes_count', 'merge_requests_count', 'upvotes', 'downvotes', 'due_date', 'confidential', 'discussion_locked', 'issue_type', 'web_url', 'time_stats', 'task_completion_status', 'blocking_issues_count', 'has_tasks', 'task_status', '_links', 'references', 'moved_to_id', 'service_desk_reply_to'])
-
-        def post_request(post_url):
-            resp = requests.post(post_url, headers=self.request_header)
-
-            if resp.status_code != 201:
-                raise Exception("Error:" + resp.text)
-
-            json_dict = json.loads(resp.text)
-            return json_dict
 
         title = urllib.parse.quote(title)              # urlencodé
         labels = urllib.parse.quote(labels)            # urlencodé
         description = urllib.parse.quote(description)  # urlencodé
 
-        gitlab_request_url = f"{self.gitlab_host}/api/v4/projects/{project_id}/issues?title={title}&labels={labels}&milestone_id={milestone_id}&description={description}"
+        gitlab_request_url = f"{self.gitlab_host}/api/v4/projects/{gitlab_project_id}/issues?title={title}&labels={labels}&milestone_id={milestone_id}&description={description}"
         print(gitlab_request_url)
 
-        resp_dict = post_request(gitlab_request_url)
+        resp = requests.post(gitlab_request_url, headers=self.request_header)
+
+        if resp.status_code != 201:
+            raise Exception("Error:" + resp.text)
+
+        resp_dict = json.loads(resp.text)
         
         return resp_dict
 
 
-    def update_issue(self):
-        pass
+    def update_issue(self, gitlab_project_id, gitlab_issue_iid, title=None, labels=None, milestone_id=None, description=None):
+        # https://docs.gitlab.com/ee/api/issues.html#edit-issue
+
+        request_params_list = []
+        if title is not None:
+            title = urllib.parse.quote(title)              # urlencodé
+            request_params_list.append(f"title={title}")
+        if labels is not None:
+            labels = urllib.parse.quote(labels)            # urlencodé
+            request_params_list.append(f"labels={labels}")
+        if milestone_id is not None:
+            request_params_list.append(f"milestone_id={milestone_id}")
+        if description is not None:
+            description = urllib.parse.quote(description)  # urlencodé
+            request_params_list.append(f"description={description}")
+
+        if len(request_params_list) > 0:
+            request_params_str = "&".join(request_params_list)
+            gitlab_request_url = f"{self.gitlab_host}/api/v4/projects/{gitlab_project_id}/issues/{gitlab_issue_iid}?{request_params_str}"
+            print(gitlab_request_url)
+
+            resp = requests.put(gitlab_request_url, headers=self.request_header)
+
+            if resp.status_code != 200:
+                raise Exception("Error:" + resp.text)
+
+            resp_dict = json.loads(resp.text)
+
+            return resp_dict
+
+        else:
+            raise ValueError("All parameters are set to None ; at least one parameter have to be set.")
 
     
-    def gitlab_get_request(self, get_url):
+    def _gitlab_get_request(self, get_url):
         """
         https://docs.gitlab.com/ee/api/issues.html
 
@@ -83,7 +107,7 @@ class GitLab:
             raise Exception("Error:" + resp.text)
         if resp.encoding is not None and resp.encoding.lower() != 'utf-8':
             raise Exception("Encoding error:", resp.encoding)
-        if resp.apparent_encoding.lower() != 'utf-8':
+        if resp.apparent_encoding.lower() not in ('utf-8', 'ascii'):
             raise Exception("Apparent_encoding error:", resp.apparent_encoding)
 
         #for issue_dict in json_list:
@@ -98,7 +122,7 @@ class GitLab:
 
         url = f"{self.gitlab_host}/api/v4/issues?per_page=100&page=1&scope=all"
 
-        json_list, resp = self.gitlab_get_request(url)
+        json_list, resp = self._gitlab_get_request(url)
         issue_list.extend(json_list)
 
         num_pages = int(resp.headers['X-Total-Pages'])
@@ -108,7 +132,7 @@ class GitLab:
             print("page {}/{}".format(page, num_pages))
 
             url = f"{self.gitlab_host}/api/v4/issues?per_page=100&page={page}&scope=all"
-            json_list, resp = self.gitlab_get_request(url)
+            json_list, resp = self._gitlab_get_request(url)
 
             issue_list.extend(json_list)
 
@@ -129,7 +153,7 @@ class GitLab:
             url_filters = "".join([f"&iids[]={iid}" for iid in iid_list])
             url = f"{self.gitlab_host}/api/v4/projects/{project_id}/issues?per_page=100&page=1&scope=all" + url_filters
 
-            json_list, resp = self.gitlab_get_request(url)
+            json_list, resp = self._gitlab_get_request(url)
             issue_list.extend(json_list)
 
             num_pages = int(resp.headers['X-Total-Pages'])
@@ -140,7 +164,7 @@ class GitLab:
                 url = f"{self.gitlab_host}/api/v4/projects/{project_id}/issues?per_page=100&page={page}&scope=all" + url_filters
 
                 print(url)
-                json_list, resp = self.gitlab_get_request(url)
+                json_list, resp = self._gitlab_get_request(url)
                 issue_list.extend(json_list)
 
         return issue_list
